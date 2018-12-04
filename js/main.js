3f01f48f4b7f9af8be5795f30b1a8464b19b555c
MalikGallow <gallowabdul@gmail.com>
2018-12-04 15:01:15 +0200

    added some javascript

--- js/main.js
+++ js/main.js
@@ -1,6 +1,12 @@
-alert("Welcome, user");
+
 
 function myFunction(){
     document.getElementById("email").innerHTML
     = "gallowabdul@gmail.com";
 }
+
+function changeImg(){
+    document.getElementById("profile").src = "images/IMG-20180828-WA0022.jpg";
+    
+}
+
